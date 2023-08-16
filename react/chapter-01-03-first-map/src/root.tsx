af0691d24e662ff17f10068869ef3c2bf25e4949
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2023-08-16 01:15:13 -0700

    Generate `chapter-01-03-first-map`

---
+++ react/chapter-01-03-first-map/src/root.tsx
@@ -0,0 +1,13 @@
+export function HackerNews() {
+  const title1 = "How Is LLaMa.cpp Possible?";
+  const title2 = "NetMaker: Connect Everything with a WireGuard VPN";
+  const title3 = "Opendream: A layer-based UI for Stable Diffusion";
+
+  return (
+    <div>
+      <p>{title1}</p>
+      <p>{title2}</p>
+      <p>{title3}</p>
+    </div>
+  );
+}
