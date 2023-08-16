2a96d365c828e9b1913266fe8bfca0044b323d04
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2023-08-16 01:20:46 -0700

    Update example code

--- react/chapter-01-03-first-map/src/root.tsx
+++ react/chapter-01-03-first-map/src/root.tsx
@@ -1,13 +1,36 @@
-export function HackerNews() {
-  const title1 = "How Is LLaMa.cpp Possible?";
-  const title2 = "NetMaker: Connect Everything with a WireGuard VPN";
-  const title3 = "Opendream: A layer-based UI for Stable Diffusion";
+const data = [
+  {
+    id: 37140013,
+    title: "How Is LLaMa.cpp Possible?",
+  },
+  {
+    id: 37142388,
+    title: "NetMaker: Connect Everything with a WireGuard VPN",
+  },
+  {
+    id: 37136898,
+    title: "Opendream: A layer-based UI for Stable Diffusion",
+  },
+  {
+    id: 37140159,
+    title: "Modern CSV version 2",
+  },
+  {
+    id: 37142426,
+    title: "Diligere, Equity-Invest Are New Firms of U.K. Con Man",
+  },
+  {
+    id: 37138807,
+    title: "How should I read type system notation?",
+  },
+];
 
+export function HackerNews() {
   return (
     <div>
-      <p>{title1}</p>
-      <p>{title2}</p>
-      <p>{title3}</p>
+      {data.map((item) => {
+        return <p key={item.id}>{item.title}</p>;
+      })}
     </div>
   );
 }
