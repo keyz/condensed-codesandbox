0f4512c5daf041b2809ca58a71edef56b7d0b2f7
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2023-08-18 02:09:05 -0700

    Add `chapter-01-10-more-styling`

---
+++ react/chapter-01-10-more-styling/src/root.tsx
@@ -0,0 +1,27 @@
+import formatDistanceToNowStrict from "date-fns/formatDistanceToNowStrict";
+import data from "./hn-top-stories.json";
+
+export function HackerNews() {
+  return (
+    <div className="flex flex-col gap-6 p-8">
+      {data.map((item) => {
+        const formattedTime = formatDistanceToNowStrict(
+          item.time * 1000, // seconds -> milliseconds
+          { addSuffix: true }, // add "ago"
+        );
+
+        return (
+          <div key={item.id}>
+            <p>
+              <a href={item.url}>{item.title}</a>
+            </p>
+            <p className="text-sm text-gray-600">
+              {item.score} points by {item.by} {formattedTime} |{" "}
+              {item.descendants} comments
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
