388fdee3c668e9d278287320804b58c5750d2c83
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2023-08-21 15:41:43 -0700

    Add `chapter-03-03-pagination-state`

---
+++ react/chapter-03-03-pagination-state/src/root.tsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+import formatDistanceToNowStrict from "date-fns/formatDistanceToNowStrict";
+import { enUS as localeEnUs } from "date-fns/locale";
+import { storyList, type TStoryItem } from "./data";
+
+const PAGE_SIZE = 10;
+
+export function HackerNews() {
+  const [currentPageNumber, setCurrentPageNumber] = React.useState<number>(0);
+
+  const startIndex = currentPageNumber * PAGE_SIZE;
+  const endIndex = (currentPageNumber + 1) * PAGE_SIZE;
+
+  const visibleWindow = storyList.slice(startIndex, endIndex);
+
+  return (
+    <div className="flex flex-col gap-6 p-8">
+      {visibleWindow.map((item) => {
+        return <NewsItem key={item.id} data={item} />;
+      })}
+    </div>
+  );
+}
+
+function NewsItem(props: { data: TStoryItem }) {
+  const { data } = props;
+
+  const [isHidden, setIsHidden] = React.useState<boolean>(false);
+
+  if (isHidden) {
+    return null;
+  }
+
+  // See https://date-fns.org/v2.30.0/docs/formatDistanceToNowStrict
+  const formattedTime = formatDistanceToNowStrict(
+    data.time * 1000, // seconds -> milliseconds
+    {
+      addSuffix: true, // add "ago"
+      locale: localeEnUs,
+    },
+  );
+
+  return (
+    <div>
+      <p>
+        <a className="hover:underline" href={data.url}>
+          {data.title}
+        </a>
+      </p>
+      <p className="text-sm text-gray-600">
+        {data.score} points by {data.by} {formattedTime}
+        {" | "}
+        <button
+          className="hover:underline"
+          onClick={() => {
+            setIsHidden(true);
+          }}
+        >
+          hide
+        </button>
+        {" | "}
+        <a
+          className="hover:underline"
+          href={`https://news.ycombinator.com/item?id=${data.id}`}
+        >
+          {data.descendants} comments
+        </a>
+      </p>
+    </div>
+  );
+}
