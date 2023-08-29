cb1f06a91ab29aca493db726c7648bbc766eb626
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2023-08-29 16:01:13 -0700

    Run lint checks separately on CI

--- react/chapter-01-09-basic-layout/next.config.js
+++ react/chapter-01-09-basic-layout/next.config.js
@@ -1,4 +1,15 @@
+// @ts-check
+
+// Vercel also sets `CI=1`
+const IS_GITHUB_CI =
+  process.env.CI === "true" && process.env.GITHUB_ACTIONS === "true";
+
 /** @type {import("next").NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Run lint checks separately on CI
+    ignoreDuringBuilds: IS_GITHUB_CI,
+  },
+};
 
 module.exports = nextConfig;
--- react/chapter-03-08-remember-unique-key/tailwind.config.js
+++ react/chapter-03-08-remember-unique-key/tailwind.config.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 /** @type {import("tailwindcss").Config} */
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
