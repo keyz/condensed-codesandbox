8f91d316a5024eb94b2a5eb1bbc84866dabeace2
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2023-08-29 19:54:59 -0700

    Update `chapter-04-01-data-fetching-starter`

--- react/chapter-04-01-data-fetching-starter/src/root.tsx
+++ react/chapter-04-01-data-fetching-starter/src/root.tsx
@@ -1,17 +1,14 @@
 import * as React from "react";
 import { PaginationControl } from "./components/pagination";
-import { storyList, type TStoryItem } from "./data";
+import { repoList, type TRepoSearchResultItem } from "./data";
 import { formatRelativeTime } from "./helpers/time";
 
 const PAGE_SIZE = 10;
 
 export function GitHubRoot() {
-  const data = storyList;
+  const data = repoList;
 
   const [currentPageNumber, setCurrentPageNumber] = React.useState<number>(0);
-  const [likeCountMap, setLikeCountMap] = React.useState<Map<number, number>>(
-    new Map(),
-  );
 
   const totalItemCount = data.length;
   const totalPageCount = Math.ceil(totalItemCount / PAGE_SIZE);
@@ -28,22 +25,7 @@ export function GitHubRoot() {
     <div className="p-8">
       <div className="flex flex-col gap-6">
         {visibleWindow.map((item) => {
-          return (
-            <NewsItem
-              key={item.id}
-              data={item}
-              likeCount={likeCountMap.get(item.id) ?? 0}
-              onLikeClick={() => {
-                setLikeCountMap((map) => {
-                  const copy = new Map(map);
-                  const currentLikeCount = copy.get(item.id) ?? 0;
-
-                  copy.set(item.id, currentLikeCount + 1);
-                  return copy;
-                });
-              }}
-            />
-          );
+          return <RepoItem key={item.id} data={item} />;
         })}
       </div>
 
@@ -63,36 +45,22 @@ export function GitHubRoot() {
   );
 }
 
-function NewsItem(props: {
-  data: TStoryItem;
-  likeCount: number;
-  onLikeClick: () => void;
-}) {
-  const { data, likeCount, onLikeClick } = props;
+function RepoItem(props: { data: TRepoSearchResultItem }) {
+  const { data } = props;
 
-  const formattedTime = formatRelativeTime(data.time);
+  const formattedCreatedAt = formatRelativeTime(data.created_at);
 
   return (
     <div>
-      <p>
-        <a className="hover:underline" href={data.url}>
-          {data.title}
+      <p className="font-medium">
+        <a className="hover:underline" href={data.html_url}>
+          {data.full_name}
         </a>
       </p>
-
+      <p>{data.description}</p>
       <p className="text-sm text-gray-600">
-        {data.score} points by {data.by} {formattedTime}
-        {" | "}
-        <a
-          className="hover:underline"
-          href={`https://news.ycombinator.com/item?id=${data.id}`}
-        >
-          {data.descendants} comments
-        </a>
-        {" | "}
-        <button className="hover:underline" onClick={onLikeClick}>
-          like ({likeCount})
-        </button>
+        {data.stargazers_count.toLocaleString("en-US")} stars | created{" "}
+        {formattedCreatedAt}
       </p>
     </div>
   );
