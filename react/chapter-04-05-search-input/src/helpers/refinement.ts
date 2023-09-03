a1d4e5bfe52cc35a5efe8d591faa989c1711de21
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2023-09-03 20:38:18 +0800

    Add `chapter-04-05-search-input`

---
+++ react/chapter-04-05-search-input/src/helpers/refinement.ts
@@ -0,0 +1,15 @@
+export function assertNonNull<T>(
+  input: T,
+  name: string,
+): asserts input is NonNullable<T> {
+  if (input == null) {
+    const error = new Error(
+      `${name}: unexpected nullish ${JSON.stringify(input)}`,
+    );
+
+    // Bonus: recapture stack trace from the callsite
+    // https://nodejs.org/api/errors.html#errorcapturestacktracetargetobject-constructoropt
+    Error.captureStackTrace(error, assertNonNull);
+    throw error;
+  }
+}
---
+++ react/chapter-04-05-search-input/src/root.tsx
@@ -0,0 +1,134 @@
+import { Octokit } from "@octokit/rest";
+import * as React from "react";
+import useSWR from "swr";
+import { assertNonNull } from "./helpers/refinement";
+import { formatRelativeTime } from "./helpers/time";
+import type { TRepoSearchResultItem } from "./types";
+
+const octokit = new Octokit({
+  auth: process.env.NEXT_PUBLIC_UNSAFE_LOCAL_ONLY_GITHUB_PERSONAL_ACCESS_TOKEN,
+});
+
+export function GitHubRoot() {
+  const [searchQ, setSearchQ] = React.useState("react+stars:>100");
+
+  return (
+    <>
+      <div className="mb-6">
+        <input
+          className="rounded-md border px-3 py-1 text-sm shadow-sm"
+          onChange={(event) => {
+            setSearchQ(event.currentTarget.value);
+          }}
+          value={searchQ}
+        />
+      </div>
+
+      <SearchResult searchQ={searchQ} />
+    </>
+  );
+}
+
+function useGitHubRepoSearchQuery(q: string) {
+  const cacheKey = ["octokit.search.repos", q];
+
+  return useSWR(cacheKey, async () => {
+    // https://docs.github.com/en/search-github/searching-on-github/searching-for-repositories
+    // https://docs.github.com/en/search-github/getting-started-with-searching-on-github/understanding-the-search-syntax
+    return await octokit.search.repos({
+      q,
+      sort: "stars",
+      order: "desc",
+      per_page: 40,
+      page: 1,
+    });
+  });
+}
+
+export function SearchResult(props: { searchQ: string }) {
+  const { searchQ } = props;
+
+  const query = useGitHubRepoSearchQuery(searchQ);
+  const response = query.data;
+
+  if (query.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (query.error != null) {
+    return <div>{String(query.error)}</div>;
+  }
+
+  if (response == null) {
+    return null;
+  }
+
+  const repoList = response.data.items;
+
+  return (
+    <div className="flex flex-col gap-6">
+      {repoList.map((item) => {
+        return <RepoItem key={item.id} data={item} />;
+      })}
+    </div>
+  );
+}
+
+function useGitHubRepoLatestReleaseQuery(input: {
+  owner: string;
+  repo: string;
+}) {
+  const { owner, repo } = input;
+  const cacheKey = ["octokit.repos.getLatestRelease", { owner, repo }];
+
+  return useSWR(cacheKey, async () => {
+    // https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#get-the-latest-release
+    return octokit.repos.getLatestRelease({
+      owner,
+      repo,
+    });
+  });
+}
+
+function RepoItem(props: { data: TRepoSearchResultItem }) {
+  const { data } = props;
+
+  assertNonNull(data.owner, "data.owner");
+
+  const query = useGitHubRepoLatestReleaseQuery({
+    owner: data.owner.login,
+    repo: data.name,
+  });
+
+  const releaseInfo =
+    query.data == null ? null : (
+      <span>
+        {" | "}
+        {query.data.data.tag_name} (released{" "}
+        {formatRelativeTime(query.data.data.created_at)})
+      </span>
+    );
+
+  const formattedCreatedAt = formatRelativeTime(data.created_at);
+
+  return (
+    <div>
+      <h2 className="font-medium">
+        <a
+          className="hover:underline"
+          href={data.html_url}
+          rel="noopener"
+          target="_blank" // Open in new tab
+        >
+          {data.full_name}
+        </a>
+      </h2>
+      <p>{data.description}</p>
+      <p className="text-sm text-gray-600">
+        {data.stargazers_count.toLocaleString("en-US")} stars | created{" "}
+        {formattedCreatedAt}
+        {releaseInfo}
+      </p>
+    </div>
+  );
+}
