89d22f1ba85ff02d2882da6a4c2d8a342a8b544a
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2023-12-20 22:47:09 -0800

    date-fns 3.x API changes

--- react/chapter-03-09-derived-value/src/helpers/time.ts
+++ react/chapter-03-09-derived-value/src/helpers/time.ts
@@ -1,5 +1,5 @@
-import formatDistanceToNowStrict from "date-fns/formatDistanceToNowStrict";
-import { enUS as localeEnUs } from "date-fns/locale";
+import { formatDistanceToNowStrict } from "date-fns/formatDistanceToNowStrict";
+import { enUS as localeEnUs } from "date-fns/locale/en-US";
 
 /**
  * See https://date-fns.org/v2.30.0/docs/formatDistanceToNowStrict
